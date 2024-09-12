9da8776b7b4c33df380a2b41f201cfa75ca1d8c2
Mehdi Sheikh <me.sheikh2024@gmail.com>
2024-09-12 17:23:18 +0330

    add files to repository

--- Src/pages/base_page.py
+++ Src/pages/base_page.py
@@ -2,15 +2,26 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
-
+from Src.utils.config  import Config
 
 
 class BasePage:
     
     def __init__(self,driver:WebDriver) :
         self.driver=driver
-        self.wait=WebDriverWait(driver,30,1,
+        self.wait=WebDriverWait(driver, Config.WAIT_TIME ,1,
                                 ignored_exceptions=[TimeoutException])
     
     def find_element(self,*locator):
         return self.wait.until(EC.visibility_of_element_located(locator))
+    
+    def click(self,*locator):
+        element =self.find_element(*locator)
+        element.click()
+        
+    def send_keys(self,text,*locator):
+        element=self.find_element(*locator)
+        element.send_keys(text)
+        
+    def get_current_url(self):
+        return self.driver.current_url
---
+++ Src/pages/login_page.py
@@ -0,0 +1,20 @@
+from base_page import BasePage
+from selenium.webdriver.common.by import By
+from Src.utils.config import Config
+
+class LoginPage(BasePage):
+    USERNAME_INPUT = (By.ID, 'username')
+    PASSWORD_INPUT = (By.ID, 'password')
+    LOGIN_BUTTON = (By.XPATH, 'x')
+
+    def __init__(self, driver):
+        super().__init__(driver)  
+        self.url = Config.LOGIN_URL  
+    
+    def click_login_button(self):
+        self.click(*self.LOGIN_BUTTON)
+        
+    def login(self,username,password):
+        self.send_keys(username,*self.USERNAME_INPUT)
+        self.send_keys(password,self.PASSWORD_INPUT)
+        self.click_login_button()
